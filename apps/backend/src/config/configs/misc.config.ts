b25e28881f32e4132988e02f981fe8c92e331602
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2024-04-16 16:47:53 +0100

    fix(backend): add missing deps

---
+++ apps/backend/src/config/configs/misc.config.ts
@@ -0,0 +1,37 @@
+import { validate } from '@config/config.validator';
+import { registerAs } from '@nestjs/config';
+import { IsEnum, IsOptional, IsPort } from 'class-validator';
+
+export interface ReCaptchaConfig {
+  secret: string;
+  verify: URL;
+  siteKey: string;
+}
+
+export enum Environment {
+  dev = 'development',
+  prod = 'production',
+  test = 'test',
+}
+
+export interface MiscConfig {
+  port: number;
+  environment: Environment;
+}
+
+class EnvVariables {
+  @IsPort()
+  @IsOptional()
+  PORT?: string;
+  @IsEnum(Environment)
+  @IsOptional()
+  NODE_ENV?: Environment;
+}
+
+export const miscConfig = registerAs('misc', (): MiscConfig => {
+  const envVariables = validate(EnvVariables);
+  return {
+    port: parseInt(envVariables.PORT ?? '3001'),
+    environment: envVariables.NODE_ENV ?? Environment.dev,
+  };
+});
