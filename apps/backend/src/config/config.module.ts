66db97bb5432b76c038c36fb0af79ed16a28a7b9
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2024-04-16 15:52:00 +0100

    feat(backend): config module

---
+++ apps/backend/src/config/config.module.ts
@@ -0,0 +1,42 @@
+import { ConfigService } from '@config/config.service';
+import * as configs from '@config/configs';
+import { DynamicModule, Global, Module } from '@nestjs/common';
+import {
+  ConfigModule as ConfigurationModule,
+  ConfigModuleOptions,
+} from '@nestjs/config';
+
+@Global()
+@Module({})
+export class ConfigModule extends ConfigurationModule {
+  static override forRoot(options?: ConfigModuleOptions): DynamicModule {
+    const defaultModule = super.forRoot({
+      envFilePath: [
+        '.env.local',
+        '.env',
+        '.env.production.local',
+        '.env.production',
+        '.env.test.local',
+        '.env.test',
+        '.env.development.local',
+        '.env.development',
+      ],
+      load: Object.keys(configs)
+        .filter((key) => typeof configs[key] === 'function')
+        .map((key) => configs[key]),
+      expandVariables: true,
+      isGlobal: true,
+      cache: true,
+      ...options,
+    });
+
+    return {
+      ...defaultModule,
+      module: ConfigModule,
+      imports: [...(defaultModule.imports || [])],
+      providers: [ConfigService, ...(defaultModule.providers || [])],
+      exports: [ConfigService],
+      global: true,
+    };
+  }
+}
---
+++ apps/backend/src/config/config.service.ts
@@ -0,0 +1,18 @@
+import { MiscConfig } from '@config/configs/misc.config';
+import { Inject, Injectable } from '@nestjs/common';
+import { ConfigService as ConfigurationService } from '@nestjs/config';
+import { CONFIGURATION_TOKEN } from '@nestjs/config/dist/config.constants';
+import { ConfigKey } from './config-key.enum';
+
+@Injectable()
+export class ConfigService extends ConfigurationService {
+  constructor(
+    @Inject(CONFIGURATION_TOKEN) defaultConfig: Record<string, unknown>,
+  ) {
+    super(defaultConfig);
+  }
+
+  getMiscConfig(): MiscConfig {
+    return this.getOrThrow<MiscConfig>(ConfigKey.misc);
+  }
+}
