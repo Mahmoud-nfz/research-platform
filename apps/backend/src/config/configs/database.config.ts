beae9a44a2cf46f73a8771da9edc47bb9dbc1806
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2024-04-20 19:37:02 +0100

    fix(backend): remove unnecssary config for database

--- apps/backend/src/config/configs/database.config.ts
+++ apps/backend/src/config/configs/database.config.ts
@@ -1,38 +1,19 @@
 import { ConfigKey } from '@config/config-key.enum';
 import { validate } from '@config/config.validator';
 import { registerAs } from '@nestjs/config';
-import { IsFQDN, IsIn, IsNotEmpty, IsPort } from 'class-validator';
+import { IsIn, IsNotEmpty } from 'class-validator';
 
 const SUPPORTED_DATABASES = ['postgres'] as const;
 type SupportedDatabases = (typeof SUPPORTED_DATABASES)[number];
 
 export interface DatabaseConfig {
   type: SupportedDatabases;
-  username: string;
-  password: string;
-  host: string;
-  port: number;
-  database: string;
-  uri: string;
+  url: string;
 }
 
 class EnvVariables {
   @IsIn(SUPPORTED_DATABASES)
   DB_TYPE: SupportedDatabases;
-  @IsNotEmpty()
-  DB_USERNAME: string;
-  @IsNotEmpty()
-  DB_PASSWORD: string;
-  @IsFQDN({
-    allow_numeric_tld: true,
-    allow_underscores: true,
-    require_tld: false,
-  })
-  DB_HOST: string;
-  @IsPort()
-  DB_PORT: string;
-  @IsNotEmpty()
-  DB_NAME: string;
   @IsNotEmpty()
   DB_URI: string;
 }
@@ -43,12 +24,7 @@ export const databaseConfig = registerAs(
     const envVariables = validate(EnvVariables);
     return {
       type: envVariables.DB_TYPE,
-      username: envVariables.DB_USERNAME,
-      password: envVariables.DB_PASSWORD,
-      host: envVariables.DB_HOST,
-      port: parseInt(envVariables.DB_PORT),
-      database: envVariables.DB_NAME,
-      uri: envVariables.DB_URI,
+      url: envVariables.DB_URI,
     };
   },
 );
