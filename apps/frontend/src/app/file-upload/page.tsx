0387c1494282095f261a40725fab40ebd2c6e5f8
Sandra Mourali <sandra.mourali@insat.ucar.tn>
2024-04-23 21:00:58 +0100

    Specify folder and bucket

--- apps/frontend/src/app/file-upload/page.tsx
+++ apps/frontend/src/app/file-upload/page.tsx
@@ -1,10 +1,12 @@
 "use client";
+import { send } from "process";
 import React, { useState } from "react";
 
 export default function Home() {
 	const [socket, setSocket] = useState<WebSocket | null>(null);
 	const [selectedFile, setSelectedFile] = useState<File | null>(null);
 	const [bucketName, setBucketName] = useState<string | null>(null);
+	const [folderName, setFolderName] = useState<string | null>(null);
 	const handleFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const file = event.target.files?.[0] || null;
 		setSelectedFile(file);
@@ -13,38 +15,43 @@ export default function Home() {
 		const name = event.target.value || null;
 		setBucketName(name);
 	}
+	const handleFolderName = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const name = event.target.value || null;
+		setFolderName(name);
+	}
 
 	const handleFileUpload = () => {
 		if (!selectedFile) return;
-
+		const updatedFileName = bucketName + "/" + selectedFile.name;
+		const updatedFile = new File([selectedFile], updatedFileName, { type: selectedFile.type });
 		const newSocket = new WebSocket("ws://localhost:1206/ws");
 
 		newSocket.onopen = () => {
 			console.log("WebSocket connection established");
-			newSocket.send(selectedFile.name);
 
+			// newSocket.send(selectedFile.name);
+			
 			const CHUNK_SIZE = 1024 * 1024;
 			let offset = 0;
 			const sendBucketName = () => {
-				if (bucketName) {
-					newSocket.send(bucketName);
-				}
+				var btt = updatedFileName +'/'+ folderName;
+				newSocket.send(btt);
+				
 			}
 			const sendChunks = () => {
-				const chunk = selectedFile.slice(offset, offset + CHUNK_SIZE);
+				const chunk = updatedFile.slice(offset, offset + CHUNK_SIZE);
 				const reader = new FileReader();
 
 				reader.onload = (e) => {
 					if (e.target?.result) {
 						const result = e.target.result;
 						if (result instanceof ArrayBuffer) {
-							// Send chunk of data
+							
 							newSocket.send(result);
 							offset += result.byteLength;
-							if (offset < selectedFile.size) {
+							if (offset < updatedFile.size) {
 								sendChunks();
 							} else {
-								// Close the WebSocket connection after all chunks have been sent
 								newSocket.close();
 							}
 						} else {
@@ -58,6 +65,8 @@ export default function Home() {
 
 				reader.readAsArrayBuffer(chunk);
 			};
+			sendBucketName();
+			// sendFolderName();
 			sendChunks();
 		};
 
@@ -70,11 +79,18 @@ export default function Home() {
 	};
 
 	return (
+		<div>
+		<div className="flex flex-col">
+			<h1>Enter Folder Name</h1>
+			<input type="text" placeholder="Enter Folder name" onChange={handleFolderName} />
+			<h1>Enter Bucket Name</h1>
+			<input type="text" placeholder="Enter Folder name" onChange={handleBucketName} />
+			</div>
 		<div className="flex flex-col">
 			<h1>WebSocket File Upload</h1>
 			<input type="file" onChange={handleFileSelect} />
-			<input type="text" placeholder="Enter bucket name" onChange={handleBucketName} />
+			
 			<button onClick={handleFileUpload}>Upload File</button>
-		</div>
+		</div></div>
 	);
 }
