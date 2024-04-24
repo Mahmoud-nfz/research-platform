010b5f08087a9a616deb45ad22e2fe2e408a5006
Sandra Mourali <sandra.mourali@insat.ucar.tn>
2024-04-24 21:41:02 +0100

    Bucket operations

---
+++ apps/frontend/src/app/bucket-operations/bucketOperationsPage.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import Upload from"./components/uploadObject";
+import ListObjects from "./components/listObjects";
+import DeleteObject from "./components/deleteObject";
+import RenameObject from "./components/renameObject";
+import CopyObjects from "./components/copyObjects";
+
+const BucketOperationsPage: React.FC = () => {
+    return (
+        <div>
+            <Upload />
+            <br />
+            <h1>*******</h1>
+            <CopyObjects />
+            <br />
+            <h1>*******</h1>
+            <ListObjects />
+            <br />
+            <h1>*******</h1>
+            <DeleteObject />
+            <br />
+            <h1>*******</h1>
+            <RenameObject />
+        </div>
+    );
+};
+
+export default BucketOperationsPage;
---
+++ apps/frontend/src/app/bucket-operations/components/uploadObject.tsx
@@ -0,0 +1,59 @@
+// Upload.tsx
+import React, { useState } from "react";
+import { handleFileUpload } from "../minioWrapperCalls";
+import { set } from "react-hook-form";
+
+const Upload: React.FC = () => {
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
+    const [bucketName, setBucketName] = useState<string | null>(null);
+    const [folderName, setFolderName] = useState<string >("");
+    const [uploadProgress, setUploadProgress] = useState<number>(0);
+    const [error, setError] = useState<string>("");
+
+    const handleFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0] || null;
+        setSelectedFile(file);
+    };
+
+    const handleBucketName = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const name = event.target.value || null;
+        setBucketName(name);
+    };
+
+    const handleFolderName = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const name = event.target.value || "";
+        setFolderName(name);
+    };
+
+    const handleUpload = () => {
+        if (!selectedFile || !bucketName ) return;
+        if (!folderName) {setFolderName('');}
+        handleFileUpload(
+            selectedFile,
+            bucketName,
+            folderName,
+            (progress) => setUploadProgress(progress),
+            (error) => setError(error)
+        );
+    };
+
+    return (
+        <div>
+            <div className="flex flex-col">
+                <h1>Enter Folder Name</h1>
+                <input type="text" placeholder="Enter Folder name" onChange={handleFolderName} />
+                <h1>Enter Bucket Name</h1>
+                <input type="text" placeholder="Enter Bucket name" onChange={handleBucketName} />
+            </div>
+            <div className="flex flex-col">
+                <h1>WebSocket File Upload</h1>
+                <input type="file" onChange={handleFileSelect} />
+                <button onClick={handleUpload}>Upload File</button>
+            </div>
+            {uploadProgress > 0 && <p>Upload Progress: {uploadProgress}%</p>}
+            {error && <p style={{ color: "red" }}>{error}</p>}
+        </div>
+    );
+};
+
+export default Upload;
