35059df5835ab12b168e367587edb4aa93bc1d6b
Omar Besbes <86571415+omar-besbes@users.noreply.github.com>
2024-04-25 01:26:59 +0100

    feat(frontend): signout

---
+++ apps/frontend/src/hooks/auth/useLogout.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useMutation } from "@tanstack/react-query";
+import { signOut, SignOutParams } from "next-auth/react";
+
+export default function useLogout() {
+  return useMutation({
+    mutationFn: async (options?: SignOutParams) => {
+      return signOut(options);
+    },
+  });
+}
