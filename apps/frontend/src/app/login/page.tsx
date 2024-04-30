3f51c5a76c260febba8fa5236ef13df73ec3f4ce
Mahmoud <mahmoudhnefzi@gmail.com>
2024-04-30 17:57:29 +0200

    feat: loading spinner onn login click

--- apps/frontend/src/app/login/page.tsx
+++ apps/frontend/src/app/login/page.tsx
@@ -13,11 +13,9 @@ import { useRouter } from "next/navigation";
 
 const loginSchema = z.object({
 	email: z.string().email({ message: "Adresse email invalide" }),
-	password: z
-		.string()
-		.min(6, {
-			message: "Mot de passe doit contenir au moins 6 caractères",
-		}),
+	password: z.string().min(6, {
+		message: "Mot de passe doit contenir au moins 6 caractères",
+	}),
 });
 type LoginSchema = z.infer<typeof loginSchema>;
 
@@ -32,7 +30,7 @@ export default function Login(): JSX.Element {
 		mode: "onTouched",
 	});
 
-	const { mutate: login, isError } = useLogin();
+	const { mutate: login, isError, isPending } = useLogin();
 
 	const onSubmit = useCallback((data: LoginSchema) => {
 		login(
@@ -44,8 +42,6 @@ export default function Login(): JSX.Element {
 			{
 				onSuccess() {
 					router.replace("/platform", { scroll: false });
-				},
-				onError() {
 				},
 			}
 		);
@@ -103,8 +99,19 @@ export default function Login(): JSX.Element {
 						<div className="flex items-center justify-center">
 							<button
 								type="submit"
-								className="mt-5 w-1/2 bg-primary-700 text-white font-semibold p-2 rounded-md hover:bg-primary-900 focus:outline-none focus:bg-black focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300"
+								disabled={isPending}
+								className="mt-5 w-1/2 bg-primary-700 flex flex-row items-center justify-around disabled:bg-gray-300 text-white font-semibold p-2 rounded-md hover:bg-primary-900 focus:outline-none focus:bg-black focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300"
 							>
+								{isPending && (
+									<div
+										className="inline-block h-4 w-4 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-[#332d2d] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+										role="status"
+									>
+										<span className="rounded-lg !absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+											Loading...
+										</span>
+									</div>
+								)}
 								Se connecter
 							</button>
 						</div>
