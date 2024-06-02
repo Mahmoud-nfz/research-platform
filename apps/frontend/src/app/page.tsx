6c3461dbac55895ad14b625ea6812e513bf11827
Mahmoud Nefzi <mahmoudhnefzi@gmail.com>
2024-06-02 20:14:37 +0100

    fix: made / redirect to /platform

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,4 +1,10 @@
+'use client';
+
 import SearchPage from '../components/general/search/SearchPage';
+import { useRouter } from 'next/navigation';
+
 export default function Home() {
+	const router = useRouter();
+	router.push('/platform');
 	return <SearchPage />;
 }
